9e9a3815c6509e1ae7daf4a860ba74fbca33c791
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-02-09 10:08:07 -0300

    feat: script modal added

---
+++ assets/JS/script.js
@@ -0,0 +1,16 @@
+const removeModal = () => {
+    var openDivModal = document.getElementById('openDivModal')
+
+    if (openDivModal.style.display = 'none') {
+        openDivModal.style.display = 'flex'
+    }
+}
+
+const closeModal = () => {
+    var openDivModal = document.getElementById('openDivModal')
+
+    if (openDivModal.style.display = 'flex') {
+        openDivModal.style.display = 'none'
+    }
+}
+
